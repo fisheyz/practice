587354b44b1a09a08d25b7ddf95eca7e7faf0b13
Fisheyz <tiago_m4t3us@hotmail.com>
2017-10-01 00:02:55 +0100

    tells if a number is even on not

--- functions.sh
+++ functions.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-function show_usage{
+function show_usage {
 	echo "Usage: $0 int"
 	exit 1
 }
 
-if [[ $1 =~ ^-?[0-9]+$ ]];then
-	(($1>5?1:2))
+
+function even_odd {
+	r=$((	$1 % 2	))
+	if [[ $r -eq 0 ]];then
+	echo "EVEN"
+else
+	echo "ODD"
+fi
+}
+
+if [[ $1  =~ ^-?[0-9]+$ ]];then
+	even_odd $1
 else
 	show_usage
 fi
